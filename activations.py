ecb444b0fbb29e4c2adbcc1712d5966783dac1f5
Игорь Дышлевский <igor11seicloud.com@Igors-MacBook-Air.local>
2022-03-24 10:58:20 +0500

    Написаны некоторые функции активации (sigmoid, relu, softmax, tanh)

---
+++ activations.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+def softmax(x):
+    return x / np.sum(x)
+
+def tanh(x):
+    return np.tanh(x)
