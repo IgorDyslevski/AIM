5ed7b732d1b576afa12782e190a0bf88072c7228
Игорь Дышлевский <igor11seicloud.com@Igors-MacBook-Air.local>
2022-03-24 16:01:14 +0500

    Сделаны аннотации типов

--- activations.py
+++ activations.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-def sigmoid(x):
+def sigmoid(x: np.array) -> np.array:
     return 1 / (1 + np.exp(-x))
 
-def relu(x):
+def relu(x: np.array) -> np.array:
     return np.maximum(0, x)
 
-def softmax(x):
+def softmax(x: np.array) -> np.array:
     return x / np.sum(x)
 
-def tanh(x):
+def tanh(x: np.array) -> np.array:
     return np.tanh(x)
