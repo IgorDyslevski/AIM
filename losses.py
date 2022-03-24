f4d5696a5f3344b9f47d7c2e6992aae458414d70
Игорь Дышлевский <igor11seicloud.com@Igors-MacBook-Air.local>
2022-03-24 16:19:41 +0500

    BugFix

--- losses.py
+++ losses.py
@@ -8,4 +8,4 @@ def mse(predicted: np.array, true: np.array) -> np.array:
     return np.sum(np.power(predicted - true, 2)) / predicted.shape[0]
 
 def bcross(predicted: np.array, true: np.array) -> np.array:
-    return -np.sum(true * np.log2(predicted)) / predicted.shape[0]
+    return -np.sum(true * np.log2(predicted) + (1 - true) * np.log2(1 - predicted)) / predicted.shape[0]
